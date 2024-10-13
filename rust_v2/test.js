e894f24b306d8ea4f2e521941f9507e54b2ba2af
ILya Saraev <saraev27@gmail.com>
2024-10-13 08:14:26 +0300

    Create test.js

---
+++ rust_v2/test.js
@@ -0,0 +1,512 @@
+{
+  "wsServerLocalAddress": "185.175.158.158",
+  "wsServerLocalPort": 9876,
+  "keysURLs": [
+    "https://raw.githubusercontent.com/NecroBoy/necroboy.github.io/refs/heads/main/aaaaa.json",
+    "https://api.npoint.io/e0ba973869231f4c3041"
+  ],
+  "reloadHours": 6,
+  "quickConnection": true,
+  "accounts": [
+    {
+      "name": "Vu8Dr2Gj6If3",
+      "password": "Jp2Po1Rv1Ii7",
+      "steamid": 76561199382212180
+    },
+    {
+      "name": "Rz7Gc2Bv1Hg7",
+      "password": "Ee1Kt3Ro6Iu5",
+      "steamid": 76561199381784925
+    },
+    {
+      "name": "As7Mf7Xh2Bm0",
+      "password": "Jk9Mj4Ip5Jp0",
+      "steamid": 76561199381881647
+    },
+    {
+      "name": "Zi4Cb1Hd7Rm9",
+      "password": "Fd6Bs8Ei2Rk7",
+      "steamid": 76561199382293782
+    },
+    {
+      "name": "In5Ly3Ia3Tl0",
+      "password": "Up5Th5Kg7Pl4",
+      "steamid": 76561199382340933
+    },
+    {
+      "name": "Gg4Qc2Eh1Zo7",
+      "password": "Pq9Lb7Mx7Vw8",
+      "steamid": 76561199381918188
+    },
+    {
+      "name": "Yt7Nt3Ga5Pv1",
+      "password": "Wl2Uu6Xn9Ai0",
+      "steamid": 76561199381989894
+    },
+    {
+      "name": "skyblueofhopeljvr",
+      "password": "qLSJMQg_",
+      "steamid": 76561199236826055
+    },
+    {
+      "name": "Up0Ln5Eq7Ze4",
+      "password": "Xb1An6Ck6Bk0",
+      "steamid": 76561199381680872
+    },
+    {
+      "name": "Qy3Dn4Mb0Ar1",
+      "password": "Ob6Rz9Re4Gn6",
+      "steamid": 76561199382080829
+    },
+    {
+      "name": "Ju0Xs8Oo3Ps3",
+      "password": "Hi0Hj4Ya7Fr1",
+      "steamid": 76561199381528492
+    },
+    {
+      "name": "Dm5Ak7Nw4Ri9",
+      "password": "Cu4Ht8Hr0Qg3",
+      "steamid": 76561199382001653
+    },
+    {
+      "name": "Dt2Pb1Lx3Mv8",
+      "password": "Ia9Na3Po8Va3",
+      "steamid": 76561199382199902
+    },
+    {
+      "name": "Pn0Na0Iy3Ev5",
+      "password": "Hm8Uf1Io9Hw7",
+      "steamid": 76561199381968396
+    },
+    {
+      "name": "sanji1dragneel1",
+      "password": "G4K9G5SM4CGX",
+      "steamid": 76561198430676274
+    },
+    {
+      "name": "Jp1Bf2Mo8Jw0",
+      "password": "Mb2Ou8Qa1Da2",
+      "steamid": 76561199381863683
+    },
+    {
+      "name": "Pj3Zx5Rl0Yb0",
+      "password": "Dk8Zu0Bi6Vy7",
+      "steamid": 76561199382310054
+    },
+    {
+      "name": "Ty3Pw7Lp0Vh2",
+      "password": "Nq6Nb5Ka2Bn2",
+      "steamid": 76561199382154389
+    },
+    {
+      "name": "Kt0Es1Nh5Pp8",
+      "password": "Wz0Jw5Tk1Yw4",
+      "steamid": 76561199382278512
+    },
+    {
+      "name": "Ou2Xs2Dv5Fk1",
+      "password": "Va6Cq3Td6Jk2",
+      "steamid": 76561199381970334
+    },
+    {
+      "name": "Yp8Qi0Cz1Hr1",
+      "password": "At6Dc4Dk1Mt8",
+      "steamid": 76561199382180479
+    },
+    {
+      "name": "Am8Ps0Oz7Tm7",
+      "password": "Fd4Bi3Qy1Dm0",
+      "steamid": 76561199382042924
+    },
+    {
+      "name": "Ck1Vu7Mb8Gn5",
+      "password": "Jm9Hh0Ye5Tv2",
+      "steamid": 76561199381780738
+    },
+    {
+      "name": "Ew7Gr8Wm3Dd1",
+      "password": "Ij8Kn1Nv1Yc5",
+      "steamid": 76561199382287236
+    },
+    {
+      "name": "Vb2Ar6Le4Sh0",
+      "password": "Jj1Pf8Uw8Od7",
+      "steamid": 76561199382214552
+    },
+    {
+      "name": "Gi1Fv2Nk9Ng9",
+      "password": "Zv2Ol4Za1Gc6",
+      "steamid": 76561199382330809
+    },
+    {
+      "name": "Cg6Rj4Tu3Qt6",
+      "password": "Ee8Ge7Za4Hh5",
+      "steamid": 76561199382189627
+    },
+    {
+      "name": "Nv5Ne7Hu1Kr9",
+      "password": "Ic8Vy0Pe0Yh4",
+      "steamid": 76561199382300376
+    },
+    {
+      "name": "Yr2Os1Nn2Xi2",
+      "password": "Fz9Jj2Jh1Ti7",
+      "steamid": 76561199382096108
+    },
+    {
+      "name": "Gi1Hn3Nc1Ze5",
+      "password": "Wg2Yz7Sr1Vj4",
+      "steamid": 76561199382065541
+    },
+    {
+      "name": "If9Us5La9Bu9",
+      "password": "Gp2If8Dy2Mw7",
+      "steamid": 76561199382069816
+    },
+    {
+      "name": "In2Ee6Il2Es9",
+      "password": "Gl6Ef3Gn9Yy1",
+      "steamid": 76561199382309708
+    },
+    {
+      "name": "Ec9Ly6Qp7Co2",
+      "password": "Bi5Vk6Pe3Tt1",
+      "steamid": 76561199382493945
+    },
+    {
+      "name": "Lh1Nb1Kd4Kb9",
+      "password": "Ox3Sf3Xs5Ba4",
+      "steamid": 76561199382395985
+    },
+    {
+      "name": "Oj3Ls3Gv6Pp8",
+      "password": "Zh5Pm3Vt8Oj9",
+      "steamid": 76561199382575700
+    },
+    {
+      "name": "Tl8Kt5Bd6Ow2",
+      "password": "Tf1Iq7Tu6Ek6",
+      "steamid": 76561199381928821
+    },
+    {
+      "name": "jlcc5901229",
+      "password": "PhgcKVEd",
+      "steamid": 76561199275790127
+    },
+    {
+      "name": "FuslUmmDl11134",
+      "password": "bD_8asNT",
+      "steamid": 76561199379315337
+    },
+    {
+      "name": "Qy0Tf5Xn1Hm2",
+      "password": "Od3My8Hv8Qb1",
+      "steamid": 76561199382305919
+    },
+    {
+      "name": "rzgenji",
+      "password": "C2B5GGFSAFEA",
+      "steamid": 76561199086833235
+    },
+    {
+      "name": "woemo230",
+      "password": "IAsioV3F",
+      "steamid": 76561199113013611
+    },
+    {
+      "name": "shanksez",
+      "password": "jj7r4ZQ0",
+      "steamid": 76561198365750476
+    },
+    {
+      "name": "Gg6Yq7Ce5Fy7",
+      "password": "Li3Kg3Uu2Vp3",
+      "steamid": 76561199382096465
+    },
+    {
+      "name": "96346490",
+      "password": "6XY4XEEBYGE3",
+      "steamid": 76561199417690212
+    },
+    {
+      "name": "Ek1He2Lz7Uh5",
+      "password": "Ve7Bk5Sl6Ce6",
+      "steamid": 76561199382311590
+    },
+    {
+      "name": "Jp7Eh3Yh5Vu9",
+      "password": "CqVY7vHw",
+      "steamid": 76561199363246837
+    },
+    {
+      "name": "Au0Ur9Sp4Ua2",
+      "password": "Al4Pa7Jt0Tt7",
+      "steamid": 76561199382058984
+    },
+    {
+      "name": "2625346502",
+      "password": "2VMjGA4A",
+      "steamid": 76561199417499240
+    },
+    {
+      "name": "erik18161",
+      "password": "rnwOR9Ni",
+      "steamid": 76561199002782206
+    },
+    {
+      "name": "Wu5Xh5Om1Zg3",
+      "password": "Oh9Cm7Zq0Zo2",
+      "steamid": 76561199382097797
+    },
+    {
+      "name": "Lk0Yy0Gd9Da1",
+      "password": "Vc1Oq2Nt7Mp7",
+      "steamid": 76561199381914482
+    },
+    {
+      "name": "Vu7Cq9Ib2Co1",
+      "password": "Ea9Gm0Oo7Dd3",
+      "steamid": 76561199382337389
+    },
+    {
+      "name": "Yc8Yw5Hc8Yo9",
+      "password": "Yh8Xa8Qa0Ro2",
+      "steamid": 76561199381781787
+    },
+    {
+      "name": "Zq2Zc6On8Aa4",
+      "password": "Zv6Rw3Oq0Wy1",
+      "steamid": 76561199381528061
+    },
+    {
+      "name": "zaqrf048",
+      "password": "JMyjAD3-",
+      "steamid": 76561199113047403
+    },
+    {
+      "name": "Bz0Sz2Df1Xx7",
+      "password": "Su2Od9Br6My8",
+      "steamid": 76561199382066305
+    },
+    {
+      "name": "dayantonio",
+      "password": "954FE6DAK9K8",
+      "steamid": 76561198273433816
+    },
+    {
+      "name": "Mu7Ae1Ec6Hy8",
+      "password": "Ed9Pc5Mi2Nk8",
+      "steamid": 76561199382201911
+    },
+    {
+      "name": "Co3La1Ug4No3",
+      "password": "Er8Lp0Lk1Ys0",
+      "steamid": 76561199382466235
+    },
+    {
+      "name": "danithekiller242",
+      "password": "hJfrOjDC",
+      "steamid": 76561199490043343
+    },
+    {
+      "name": "We0Xy0Rt7Zr2",
+      "password": "Ji3Cs6Rh4Tw7",
+      "steamid": 76561199382315776
+    },
+    {
+      "name": "Xe6Bm8Hz4Es7",
+      "password": "We5Ka0Pq2Ea9",
+      "steamid": 76561199381759936
+    },
+    {
+      "name": "Ri0Vr6Ep7Jm2",
+      "password": "Pe5Ic1Zc9Ji5",
+      "steamid": 76561199382403545
+    },
+    {
+      "name": "ij9jj3js7ol2",
+      "password": "RX8x1p-n",
+      "steamid": 76561199345747857
+    },
+    {
+      "name": "Ye9Zj4Yg1By7",
+      "password": "Vn3Tm3Yr4Mk6",
+      "steamid": 76561199382134364
+    },
+    {
+      "name": "Hb6Sx0Mp1Ft2",
+      "password": "Xy0Yi3Bn6Yb5",
+      "steamid": 76561199382100571
+    },
+    {
+      "name": "Ik5Yx7Ev6Dl2",
+      "password": "On4Hj0Me1Gm5",
+      "steamid": 76561199382420290
+    },
+    {
+      "name": "Td2Xm8Za0Ae7",
+      "password": "El3Wu7Ql0Zl1",
+      "steamid": 76561199381946370
+    },
+    {
+      "name": "jimmynigglet",
+      "password": "5X4TT3CTKDB7",
+      "steamid": 76561199657193680
+    },
+    {
+      "name": "Nimo1215",
+      "password": "ZXBMSFBGDXEG",
+      "steamid": 76561198892536125
+    },
+    {
+      "name": "megaxit1",
+      "password": "yPlackaQ",
+      "steamid": 76561198196523179
+    },
+    {
+      "name": "Bc5Wk5Os2Oo6",
+      "password": "Bd3De0Jm1Al3",
+      "steamid": 76561199381936190
+    },
+    {
+      "name": "Px5Pr7Cv5Vc2",
+      "password": "Qk1Vm7Dl4Nq0",
+      "steamid": 76561199382270622
+    },
+    {
+      "name": "Oz8Ll5Dz6Rr0",
+      "password": "Rx1On8Gc0Qi1",
+      "steamid": 76561199382113350
+    },
+    {
+      "name": "Nt4Og1Mz5Xq2",
+      "password": "De3Ly9Hz6Nu0",
+      "steamid": 76561199382127375
+    },
+    {
+      "name": "nenuvuwatijasta",
+      "password": "e6sE-wF3",
+      "steamid": 76561199436731212
+    },
+    {
+      "name": "nemo111110",
+      "password": "TnA9TReN",
+      "steamid": 76561198437798996
+    },
+    {
+      "name": "y0407ang",
+      "password": "JxRFFuSZ",
+      "steamid": 76561199136081433
+    },
+    {
+      "name": "richardxrobertsojh",
+      "password": "9Zz-NDVY",
+      "steamid": 76561199547151772
+    },
+    {
+      "name": "Pn7Cf3Cw5Ik0",
+      "password": "Gs3Gf7Am8Nj9",
+      "steamid": 76561199382349403
+    },
+    {
+      "name": "jirvok608",
+      "password": "HDHDfqhI",
+      "steamid": 76561198126788059
+    },
+    {
+      "name": "Ev1Rt8Kz5Pn7",
+      "password": "Ou9Jb4Nh4Ko4",
+      "steamid": 76561199382248732
+    },
+    {
+      "name": "yqkr380905",
+      "password": "Dca-o9qW",
+      "steamid": 76561199161589121
+    },
+    {
+      "name": "Yt1Ec5Vb9Tj7",
+      "password": "Tc3Dl1Tv2Zx7",
+      "steamid": 76561199382047988
+    },
+    {
+      "name": "Ub9Cu4Ja9Nj1",
+      "password": "Dm3Hd3Rx5Zh5",
+      "steamid": 76561199381859817
+    },
+    {
+      "name": "Qp1Dm7Ts6Wc8",
+      "password": "Kn2Gw9Nu7Zt5",
+      "steamid": 76561199382059520
+    },
+    {
+      "name": "Oo0Iw0Hb1At7",
+      "password": "Ag8Vn6Gj1We2",
+      "steamid": 76561199382103419
+    },
+    {
+      "name": "At2Pv5Rk2Py7",
+      "password": "Dh7Yu6Nz7Dl4",
+      "steamid": 76561199381921765
+    },
+    {
+      "name": "ffyai821",
+      "password": "wJ7YPS-G7VcW",
+      "steamid": 76561199113339862
+    },
+    {
+      "name": "Xt7Xl3Za1Du1",
+      "password": "Bm6On2Si9Tp6",
+      "steamid": 76561199382042803
+    },
+    {
+      "name": "xflypvp",
+      "password": "hfJU83FA",
+      "steamid": 76561198182308789
+    },
+    {
+      "name": "Cd7Po6Us6Zj5",
+      "password": "Nd8Rp9Ef1Hq8",
+      "steamid": 76561199382382834
+    },
+    {
+      "name": "Wn1Ll5Wl4Fk2",
+      "password": "Ib6Oz4Ln8Uh3",
+      "steamid": 76561199382323388
+    },
+    {
+      "name": "pvfk04572",
+      "password": "3MCS892AFBTE",
+      "steamid": 76561199410868789
+    },
+    {
+      "name": "Ye8Ch9Gz0Cd1",
+      "password": "Jj2Nw9Yl6Gs2",
+      "steamid": 76561199382229224
+    },
+    {
+      "name": "Oo3Xv2Ty5Fe7",
+      "password": "Cz4Id1Pu3Rv6",
+      "steamid": 76561199382396241
+    },
+    {
+      "name": "Sx7Yq3Vi9Ij2",
+      "password": "Xg8Zy6Lr7Ss5",
+      "steamid": 76561199381931000
+    },
+    {
+      "name": "Mh2Hq3Rv4Wp2",
+      "password": "Cg2Vp1Pf7Mz0",
+      "steamid": 76561199382672772
+    },
+    {
+      "name": "Tk7Xz6Rc3Tu8",
+      "password": "Wd4Za2Wl2Kv7",
+      "steamid": 76561199381901365
+    },
+    {
+      "name": "Ci3Tl5Ws9Nc5",
+      "password": "Ie8Cf2Aq7Os1",
+      "steamid": 76561199382185869
+    }
+  ]
+}
